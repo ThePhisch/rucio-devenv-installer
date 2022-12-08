37db3af05715e77f9ebf1ef029aa8ac939704881
Anton Schwarz <anton.schwarz@cern.ch>
2022-12-08 17:52:39 +0900

    Added checks if things are already installed, more structure

--- install.sh
+++ install.sh
@@ -1,35 +1,79 @@
-MINSTALL_REPO='htps://github.com/ThePhisch/rucio-vscode-dev-env'
+MINSTALL_REPO='https://github.com/ThePhisch/rucio-vscode-dev-env'
 RUCIO_REPO='https://github.com/rucio/rucio.git'
 VSCODE_SOURCE='https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64'
 
-# Docker install
-# TODO: check if already installed
+function bigwrite() {
+  echo ""
+  echo ""
+  echo "=============================================================="
+  echo "$1"
+  echo "=============================================================="
+}
+
+# Packages install
+bigwrite "Installing Other Dependencies"
 sudo apt-get update
 sudo apt-get install \
     ca-certificates \
     curl \
     gnupg \
-    lsb-release
+    lsb-release \
+    git
 
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+# Docker install
+bigwrite "Installing Docker"
+DOCKERPACKAGES=("docker-ce" "docker-ce-cli" "containerd.io" "docker-compose-plugin")
+ALLINSTALLED=1
 
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+for pname in "${DOCKERPACKAGES[@]}"; do
+  if [[ -z $(dpkg-query -s $pname | grep 'Status: install ok installed') ]]; then
+    echo "$pname NOT installed"
+    ALLINSTALLED=0
+  else
+    echo "$pname installed"
+  fi
+done
 
-sudo apt-get update
+if [[ $ALLINSTALLED -eq 1 ]]; then
+  echo "All installed"
+else
+  echo "Not all installed"
+  sudo mkdir -p /etc/apt/keyrings
+  curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
-sudo usermod -aG docker $USER
+  echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-# Git install
-sudo apt-get install git
+  sudo apt-get update
+  sudo apt-get install ${DOCKERPACKAGES[@]}
+
+  sudo usermod -aG docker $USER
+fi
+
+
 
 # VSC Check Addons
-# TODO: do this
+bigwrite "VS Code Extensions"
+if [[ -n $(code --list-extensions | grep ms-azuretools.vscode-docker) ]]; then
+  VSC_DOCKER_VERSION=$(code --list-extensions --show-versions | grep ms-azuretools.vscode-docker | awk -F@ '{print $2}')
+  echo "Found VS Code Docker Extension, version $VSC_DOCKER_VERSION"
+
+  if [[ -z  $(echo $VSC_DOCKER_VERSION | awk -F. '{if ($1 >= 1 && $2 >= 23) print }') ]]; then
+    echo "Version is below 1.23, Updating..."
+    code --install-extension ms-azuretools.vscode-docker --force --wait
+  else
+    echo "Version is okay"
+  fi
+else
+  echo "Did not find VS Code Docker Extension, Installing..."
+  code --install-extension ms-azuretools.vscode-docker --force --wait
+fi
+echo "Dealing with VS Code Extensions Completed!"
+
 
 # Folders and cloning
+bigwrite "Creating Folder Structure and Cloning the Testenv Repo"
 cd ~
 mkdir dev
 cd dev
@@ -42,6 +86,7 @@ mv rucio-vscode-dev-env/* .
 rm -rf rucio-vscode-dev-env
 
 # moving keys
+bigwrite "Moving certificates around"
 cd ~/dev/rucio
 cp etc/certs/hostcert_rucio.key.pem .vscode/certs/hostkey.pem
 cp etc/certs/hostcert_rucio.pem .vscode/certs/hostcert.pem
